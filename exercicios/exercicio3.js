f2bab1faf2b8c10103d7b35066b3abd285241bae
myrmanso <mayaramanso.devel@gmail.com>
2019-04-12 17:27:35 -0300

    página palindromo e xercicio de lógica com js

--- exercicios/exercicio3.js
+++ exercicios/exercicio3.js
@@ -11,11 +11,28 @@ Declare uma variável `pessoa`, que receba as suas informações pessoais, segui
 - `caminhouQuantosMetros` - Number - recebe "zero" por padrão
 */
 
+var pessoa = {
+    nome: 'Mayara Cristina',
+    sobrenome: 'Manso',
+    idade: 25,
+    altura: 1.57,
+    reprograma: 'Front-end',
+    andando: false,
+    caminhouQuantosMetros: 0,
+}
+
+
 /*
 Adicione um método ao objeto `pessoa` chamado `fazerAniversario`. O método deve
 alterar o valor da propriedade `idade` dessa pessoa, somando `1` a cada vez que
 for chamado.
 */
+
+pessoa.fazerAniversário = () => {
+    pessoa.idade++;
+}
+
+
 
 /*
 Adicione um método ao objeto `pessoa` chamado `andar`, que terá as seguintes
@@ -28,46 +45,89 @@ valor dessa propriedade a quantidade passada por parâmetro;
 booleano que representa "verdadeiro";
 */
 
+function andar(){
+    var caminhar = prompt("Você caminhou?");
+
+    if(caminhar == "sim"){
+        var metros = parseInt(prompt("Quantos metros?"));
+        pessoa.caminhouQuantosMetros += metros;
+        document.write("Você andou " + pessoa.caminhouQuantosMetros)
+    } else {
+        document.write("Você não caminhou, ficou em casa danada!")
+    }
+}
+
 /*
 Adicione um método ao objeto `pessoa` chamado `parar`, que irá modificar o valor
 da propriedade `andando` para o valor booleano que representa "falso".
 */
+
+
 
 /*
 Crie um método chamado `meuNomeCompleto`, que retorne a frase:
 - "Meu nome é [NOME] [SOBRENOME]"
 */
 
+pessoa.nomeCompleto = () => {
+    
+    console.log(pessoa.nome + " " + pessoa.sobrenome)
+}
+
 /*
 Crie um método chamado `mostrarIdade`, que retorne a frase:
 - "eu tenho [IDADE] anos"
 */
+
+pessoa.mostrarIdade = () => {
+    console.log("Eu tenho " + pessoa.idade + " anos.")
+}
 
 /*
 Crie um método chamado `mostrarAltura` que retorne a frase:
 - "minha altura é [ALTURA]"
 */
 
+pessoa.mostrarAltura = ()  => {
+    console.log("Minha altura " + pessoa.altura + " .")
+}
+
 /*
 Agora vamos mostrar as informações do objeto criado!
 Qual o nome completo da pessoa? 
 */
 
+pessoa.nomeCompleto()
+
 /*
 Qual a idade da pessoa? 
 */
+
+pessoa.mostrarIdade()
 
 /*
 Qual a altura da pessoa?
 */
 
+pessoa.mostrarAltura()
+
 /*
 Faça a `pessoa` fazer 3 aniversários.
 */
-
 /*
 Quantos anos a `pessoa` tem agora? 
 */
+
+var cont = 0;
+
+while (cont <4){
+    fazerAniversário();
+    cont++;
+}
+
+document.write("Agora você tem " + fazerAniversário())
+
+
 
 /*
 Agora, faça a `pessoa` caminhar alguns metros, invocando o método `andar` 3x,
